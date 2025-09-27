1751bece125d390c9847ae53e790d951ff378ed6
Aditya Kalambe <adityakalambe20@gmail.com>
2025-09-28 01:37:03 +0530

    feat(hotels): add TabsModule to HotelsComponent

--- src/app/super-admin/hotels-component/hotels-component.ts
+++ src/app/super-admin/hotels-component/hotels-component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {CardModule} from '../../shared/ui/card/card.module';
 import {LucidIconWrapper} from '../../shared/components/lucid-icon-wrapper/lucid-icon-wrapper';
 import {icons} from '../../core/constants';
+import {TabsModule} from '../../shared/ui/tabs/tabs.module';
 
 @Component({
   selector: 'app-hotels-component',
   imports: [
     CardModule,
-    LucidIconWrapper
+    LucidIconWrapper,
+    TabsModule
   ],
   templateUrl: './hotels-component.html',
   styleUrl: './hotels-component.css'
