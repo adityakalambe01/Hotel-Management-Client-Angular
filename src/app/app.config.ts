cb839e4404348e81a08f8962d90d8c56e1bc1e73
AdityaAngular01 <aditya@angularminds.in>
2025-09-29 12:43:08 +0530

    feat(ngrx): add store and effects providers to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,16 @@ import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {baseUrlInterceptor} from './core/interceptors/Base URL/base-url-interceptor';
 import {authInterceptor} from './core/interceptors/auth/auth-interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([baseUrlInterceptor, authInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([baseUrlInterceptor, authInterceptor])),
+    provideStore(),
+    provideEffects()
+]
 };
