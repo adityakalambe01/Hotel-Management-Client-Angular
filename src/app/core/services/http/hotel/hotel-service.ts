eefdd7f40fe2b94cfb092302eb73d6ef8b7b67c7
AdityaAngular01 <aditya@angularminds.in>
2025-09-29 12:44:43 +0530

    feat(hotel): add HotelService with hotel and category API methods

---
+++ src/app/core/services/http/hotel/hotel-service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClientService} from '../http-client-service';
+import {Observable} from 'rxjs';
+import {IHttpSuccessResponse} from '../../../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelService extends HttpClientService{
+  private hotelPrefixURL = '/hotel';
+  private hotelCategoryPrefixURL = '/hotel-category';
+
+  // Hotel routes
+  createHotel(data:any): Observable<IHttpSuccessResponse<any>>{
+    return this.post<typeof data, IHttpSuccessResponse<any>>(`${this.hotelPrefixURL}`, data);
+  }
+
+  // Hotel Categories
+  getHotelCategories(): Observable<IHttpSuccessResponse<any>>{
+    return this.get<IHttpSuccessResponse<any>>(`${this.hotelCategoryPrefixURL}`);
+  }
+}
