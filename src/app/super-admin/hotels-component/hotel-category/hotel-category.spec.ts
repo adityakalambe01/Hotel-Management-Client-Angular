00b4e1a51fd283c70820637fadc902e2ba484b9f
AdityaAngular01 <aditya@angularminds.in>
2025-09-29 12:43:54 +0530

    feat(hotel): add HotelCategory component with service integration

---
+++ src/app/super-admin/hotels-component/hotel-category/hotel-category.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HotelCategory } from './hotel-category';
+
+describe('HotelCategory', () => {
+  let component: HotelCategory;
+  let fixture: ComponentFixture<HotelCategory>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HotelCategory]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HotelCategory);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/super-admin/hotels-component/hotel-category/hotel-category.ts
@@ -0,0 +1,45 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {HotelService} from '../../../core/services/http/hotel/hotel-service';
+import {IHttpErrorResponse, IHttpSuccessResponse} from '../../../core/models';
+import {CardModule} from '../../../shared/ui/card/card.module';
+import {LucidIconWrapper} from '../../../shared/components/lucid-icon-wrapper/lucid-icon-wrapper';
+import {icons} from '../../../core/constants';
+import {Subscription} from 'rxjs';
+import {Utils} from '../../../core/utils/utils';
+
+@Component({
+  selector: 'app-hotel-category',
+  imports: [CardModule, LucidIconWrapper],
+  templateUrl: './hotel-category.html',
+  styleUrl: './hotel-category.css'
+})
+export class HotelCategory implements OnInit, OnDestroy{
+  protected icons = icons
+  protected hotelCategories: any[] = [];
+  private hotelCategorySubscription!:Subscription;
+  constructor(private hotelService: HotelService, private utils:Utils) {
+  }
+  ngOnInit() {
+    this.hotelCategorySubscription = this.hotelService.getHotelCategories().subscribe({
+      next: (response: IHttpSuccessResponse)=>{
+        this.hotelCategories = response.data.results;
+      },
+      error: (error: IHttpErrorResponse)=>{
+        console.log(error)
+      },
+      complete: ()=>{
+
+      }
+    })
+  }
+
+  getIcon(name: string) {
+    return this.utils.getIcon(icons.hotelCategory, name)
+  }
+
+
+  ngOnDestroy() {
+    this.hotelService.unsubscribe(this.hotelCategorySubscription)
+  }
+
+}
